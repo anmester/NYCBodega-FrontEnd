79bfddcd1c615eb86b24ce5762f6a20331ef8501
anmester <github email address>
2019-08-19 11:08:56 -0400

    updated review cards

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import '../App.css';
 import 'semantic-ui-css/semantic.min.css'
+import { Button, Card, Image } from 'semantic-ui-react'
 
 export default class Review extends React.Component{
     render() {
+        console.log('users bodegas', this.props.bodegas)
+        let targetBodega = this.props.bodegas.filter(bodega => bodega.id === this.props.review.bodega_id)
         return(
-            <div className='review-card'>
-                <h2>Inside Review</h2>
-                <p>{this.props.review.beernumber}</p>
-            </div>
+            <Card className='review-card'>
+                <Card.Content>
+                    <Card.Header>{targetBodega[0].name}</Card.Header>
+                    <Card.Description>
+                        <h4>Overall Rating: {this.props.review.overallreview}</h4>
+                        <strong>Beer Rating:</strong> {this.props.review.beernumber ? this.props.review.beernumber : 'no rating provided'}<br></br>
+                        <strong>Beer Description:</strong> {this.props.review.beertext ? this.props.review.beertext : 'no comment provided'}<br></br><br></br>
+                        <strong>Cat Rating:</strong> {this.props.review.catnumber ? this.props.review.catnumber : 'no rating provided'}<br></br>
+                        <strong>Cat Description:</strong> {this.props.review.cattext ? this.props.review.cattext : 'no comment provided'}<br></br><br></br>
+                        <strong>Coffee Rating:</strong> {this.props.review.coffeenumber ? this.props.review.coffeenumber : 'no rating provided'}<br></br>
+                        <strong>Coffee Description:</strong> {this.props.review.coffeetext ? this.props.review.coffeetext : 'no comment provided'}<br></br><br></br>
+                        <strong>Sandwich Rating:</strong> {this.props.review.sandwichnumber ? this.props.review.sandwichnumber : 'no rating provided'}<br></br>
+                        <strong>Sandwich Description:</strong> {this.props.review.sandwichtext ? this.props.review.coffeetext : 'no comment provided'}<br></br><br></br>
+                        <strong>Snacks Rating:</strong> {this.props.review.snacksnumber ? this.props.review.snacksnumber : 'no rating provided'}<br></br>
+                        <strong>Snacks Description:</strong> {this.props.review.snackstext ? this.props.review.snackstext : 'no comment provided'}<br></br><br></br>
+                        <strong>Hours Rating:</strong> {this.props.review.hoursnumber ? this.props.review.hoursnumber : 'no rating provided'}<br></br>
+                        <strong>Hours Description:</strong> {this.props.review.hourstext ? this.props.review.hourstext : 'no comment provided'}<br></br><br></br>
+                        <strong>Late Night?</strong> {this.props.review.latenight ? 'yes!' : 'no :('}
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content>
+                    <button class="ui primary button">Edit Review</button>
+                    <button class="ui secondary button">Delete Review</button>
+                </Card.Content>
+            </Card>
         )
     }
 }
--- src/containers/MyReviews.js
+++ src/containers/MyReviews.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 import 'semantic-ui-css/semantic.min.css'
+import { Button, Card, Image } from 'semantic-ui-react'
 import Review from '../components/Review'
 import NavBar from '../components/NavBar'
 
@@ -15,19 +16,21 @@ export default class MyReviews extends React.Component{
         fetch('http://localhost:3000/userreviews/2')
          .then(resp => resp.json())
          .then(apiData =>
-          this.setState({ reviews: apiData[0], bodegas: apiData.splice(0,1) })
+          this.setState({ reviews: apiData[0], bodegas: apiData.slice(1, apiData.length) })
          );
     }
     
     render() {
         console.log('bodegas', this.state.bodegas)
         console.log('reviews', this.state.reviews)
-        let reviewArray = this.state.reviews.map(review => <Review key={review.id} review={review}/>)
+        let reviewArray = this.state.reviews.map(review => <Review key={review.id} review={review} bodegas={this.state.bodegas}/>)
         return(
-            <div className='my-reviews'>
+            <React.Fragment>
                 <h2>My Reviews!</h2>
-                {reviewArray}
-            </div>
+                <Card.Group className='my-reviews' itemsPerRow='3'>
+                    {reviewArray}
+                </Card.Group>
+            </React.Fragment>
         )
     }
 }
