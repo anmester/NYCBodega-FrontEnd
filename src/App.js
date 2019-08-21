a5e6843fa2caa449cd2f6dbccb942e36ba636a8f
anmester <github email address>
2019-08-21 00:23:01 -0400

    added logout functionality

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
     signUpSubmit = (e, user) => {
       e.preventDefault()
       console.log('Sign up user:', user)
+      localStorage.clear()
       let username = user.username
       let displayname = user.displayname
       let neighborhood = user.neighborhood
